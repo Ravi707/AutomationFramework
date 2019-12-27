ebc6065debcabec6cc4c606eb655950a43cbccbd
raviq <raviq@192.168.2.5>
2019-12-27 22:02:42 +0530

    utility method is under progress

---
+++ src/test/java/com/FrameWork_CommonMethods/Excel_Utilities.java
@@ -0,0 +1,24 @@
+package com.FrameWork_CommonMethods;
+
+import java.util.Date;
+
+public class Excel_Utilities {
+
+	public static void main(String[] args) {
+
+		/*String pp = System.getProperty("user.dir");
+		System.out.println(pp);
+		Date d = new Date();
+		System.out.println(d);
+		System.out.println(d.toString().replace(":", "_").replace(" ", "_"));*/
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
